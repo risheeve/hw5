4374107f339c23065237a1f28ba9085154fe05dc
Rishee Venkatesh <risheeve@usc.edu>
2024-04-20 20:02:51 +0000

    Removed tests

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool schedValidity(const AvailabilityMatrix& avail, map<Worker_T, int>& workShifts, size_t maxShifts, int dayIndx, Worker_T worker);
+bool scheduleDay(size_t dayInd, const AvailabilityMatrix& avail, DailySchedule& sched, map<Worker_T, int> workShifts, size_t maxShifts, size_t dailyNeed);
+
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -32,14 +35,66 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if(avail.size() == 0U|| avail[0].empty()){
         return false;
     }
+    size_t days = avail.size();
+    size_t workers = avail[0].size();
+    sched.resize(days, vector<Worker_T>(dailyNeed));
     sched.clear();
+    sched.resize(days);
+    map<Worker_T, int> workShift;
+    return scheduleDay(0, avail, sched, workShift, maxShifts, dailyNeed);
+}
+
     // Add your code below
+    bool scheduleDay(size_t dayIndx, const AvailabilityMatrix& avail, DailySchedule& sched, map<Worker_T, int> workShifts, size_t maxShifts, size_t dailyNeed) {
+    if (dayIndx == avail.size()) {
+        return true;  
+    }
+    size_t workers = avail[dayIndx].size();
+    for (Worker_T i = 0; i < workers; ++i) {
+         
+        if (schedValidity(avail, workShifts, maxShifts, dayIndx, i)) {
+            sched[dayIndx].push_back(i);
+            if(sched[dayIndx].size() == dailyNeed) {
+                if(scheduleDay(dayIndx+1, avail, sched, workShifts, maxShifts, dailyNeed)) {
+                    return true;
+                }
+                sched[dayIndx].pop_back();
+                workShifts[i] -= 1;
+            }
+            else {
+                if(scheduleDay(dayIndx, avail, sched, workShifts, maxShifts, dailyNeed)) {
+                    return true;
+                }
+                sched[dayIndx].pop_back();
+                workShifts[i] -= 1;
+            }
 
+        }
+        
+    }
 
-
+    return false;
 
 }
+
+bool schedValidity(const AvailabilityMatrix& avail, map<Worker_T, int>& workShifts, size_t maxShifts, int dayIndx, Worker_T worker){
+    if(!avail[dayIndx][worker]) {
+        return false;
+    }
+    int shifts = workShifts[worker];
+    if(shifts >= maxShifts) {
+        return false;
+    } else {
+        workShifts[worker] += 1;
+        return true;
+    }
+
+}
+
+
+
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,8 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
+#include <string>
 #endif
 
 #include "wordle.h"
@@ -13,7 +15,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void recursiveHelper(string& currWord, int indx, const string& in, vector<int>& xFloating, const string& floating, set<string>& results, const set<string>& dict);
+bool wordValidity(const string& word, const set<string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +25,66 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    string currWord = in;
+    vector<int> xFloating(floating.length(), 0); 
+    set<string> results;
+    recursiveHelper(currWord, 0, in, xFloating, floating, results, dict);
+    return results;
 
 }
 
-// Define any helper functions here
+//Define any helper functions here
+void recursiveHelper(string& currWord, int indx, const string& in, vector<int>& xFloating, const string& floating, set<string>& results, const set<string>& dict) {
+    if (indx == currWord.size()) {
+        for (int count : xFloating) {
+            if (count == 0) {
+              return;
+            }
+        }
+        if (wordValidity(currWord, dict)) {
+            results.insert(currWord);
+        } return;
+    }
+    int dashes = 0;
+    int floatLeft = 0;
+    for(int i = 0; i < xFloating.size(); i++) {
+        if(xFloating[i] == 0) {
+            floatLeft++;
+        }
+    }
+    for(int i = 0; i < currWord.size(); i++) {
+        if(currWord[i] == '-') {
+            dashes++;
+        }
+    }
+    if(dashes < floatLeft) {
+        return;
+    }
+    if (currWord[indx] != '-') {
+        recursiveHelper(currWord, indx + 1, in, xFloating, floating, results, dict);
+    } else {
+        int index = 0;
+        for (char c = 'a'; c <= 'z'; ++c) {
+            currWord[indx] = c;
+            bool isFloat = false;
+            for (size_t i = 0; i < floating.size(); ++i) {
+                if (floating[i] == c && xFloating[i] < 1) {
+                    xFloating[i]++;
+                    index = i;
+                    isFloat = true;
+                    break;
+                }
+            }
+            recursiveHelper(currWord, indx + 1, in, xFloating, floating, results, dict);
+            if (isFloat) {
+                xFloating[index]--;
+            }
+            currWord[indx] = '-';
+        }
+    }
+}
+
+bool wordValidity(const string& word, const set<string>& dict) {
+    return dict.find(word) != dict.end();
+}
+
